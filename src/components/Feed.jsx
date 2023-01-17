833bd55743ba3b2d313b09757a4ba7f337af0dc4
Akshay <erakshayjadhav8@gmail.com>
2023-01-18 00:12:23 +0530

    done with sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar, Videos } from "../components";
+
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row", backgroundColor: '#5c5c5c' } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: {    sx: 0, md: 2 }}}>
+        <Sidebar/>
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: 'whitesmoke' }}>
+          CopyRight 2022
+        </Typography>
+
+      </Box>
+
+      <Box p={1} sx={{ overflowY: 'auto', height: '90vh', flex: '2' }}>
+        <Typography variant="h4" mb={2} sx={{ color: 'white' }}> 
+          New <span style={{ color: '#FC1503' }}>videos</span>
+        </Typography>
+
+        <Videos videos={[]}/>
+
+      </Box>
+
+    </Stack>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,6 @@ export { default as Feed } from './Feed';
 export { default as ChannelDetails } from './ChannelDetails';
 export { default as SearchFeed } from './SearchFeed';
 export { default as VideoDetails } from './VideoDetails';
+export { default as SearchBar } from './SearchBar';
+export { default as Sidebar } from './Sidebar';
+export { default as Videos } from './Videos';
