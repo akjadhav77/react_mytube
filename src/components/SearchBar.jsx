4e4fca58df630fe87684ebd93179094ee4b16de1
Akshay <erakshayjadhav8@gmail.com>
2023-01-24 17:33:11 +0530

    done with channel profile

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,25 @@ import { Paper, IconButton } from '@mui/material'
 import { Search } from "@mui/icons-material"
 
 const SearchBar = () => {
-  return (
+  
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+
+    if (searchTerm) {
+        navigate(`/search/${searchTerm}`);
+        setSearchTerm('');
+    }
+
+  }
+
+    return (
     <Paper
         component="form"
-        onSubmit={()=> {}}
+        onSubmit={handleSubmit}
         sx={{
             borderRadius: 3,
             border: '1px solid #e3e3e3',
@@ -16,7 +31,7 @@ const SearchBar = () => {
             mr: { sm: 5 }
         }}
     >
-        <input className="search-bar" placeholder="Search..." value="" onChange={()=> {}}/>
+        <input className="search-bar" placeholder="Search..." value="searchTerm" onChange={(e)=> setSearchTerm(e.target.value)}/>
         <IconButton>
             <Search type="submit" sx={{ p: '1px', color: 'red' }}/>
         </IconButton>
